11db5eef55fe8eb92ebdc9c3d8cd3993ff61a917
CleanSand <kagami228k@mail.ru>
2023-11-13 22:49:06 +0700

    Docker

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,6 +11,7 @@ class ProductController extends Controller
 
     public function index()
     {
+        dd(Product::all());
         return Product::all();
     }
 
--- docker/Nginx.Dockerfile
+++ docker/Nginx.Dockerfile
@@ -2,3 +2,5 @@ FROM nginx
 ADD docker/conf/vhost.conf /etc/nginx/conf.d/default.conf
 
 WORKDIR /var/www/html
+
+COPY . .
